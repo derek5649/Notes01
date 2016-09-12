70349e304b41e894d918bf4d00edc572d2e7dea6
Derek Hall <dhall68@mail.valenciacollege.edu>
2016-09-12 14:59:38 +0000

    updated notes

---
+++ Notes01/index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+   <title>hello-world php</title>     
+    </head>
+    <body>
+        
+        <p>hello-world</p>
+        
+        <?php 
+        echo("hello-world");
+        ?>
+        <script type="text/javascript">
+            document.write("hello-world");
+        </script>
+        
+    </body>
+</html>
